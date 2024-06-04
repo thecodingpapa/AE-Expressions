41101fa800dd7233c3cd6432ba4907d7c26a8995
thecodingpapa <yangfamilyclub@gmail.com>
2024-06-04 14:49:32 +0100

    first version

--- create graph base.js
+++ create graph base.js
@@ -5,8 +5,39 @@ var numberOfMiddleLines = 3;
 var margin = 100;
 var strokeWidth = 4;
 var strokeColor = [0, 0, 0]; // Black color
+
 var textYPosFromBottomLine = 30;
 var xValues = ["1990", "1995", "2000", "2005", "2010", "2015", "2020"];
+
+var textXPosFromLeftLine = 20;
+var yValues = ["0", "20", "40", "60", "80", "100"];
+
+
+
+
+
+/***
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ * DO NOT TOUCH THE CODE BELOW!!!!!!!!!!!!!!
+ */
 
 // Create a new composition
 var comp = app.project.items.addComp("Graph Base Lines", 1920, 1080, 1, 10, 30);
@@ -58,10 +89,18 @@ function createDottedLine(startPoint, endPoint, lineName, dashLength, gapLength)
     gap.setValue(gapLength); // Gap length
 }
 
-function createText(position, textContent, textName) {
+function createText(position, textContent, textName, justification) {
     var textLayer = comp.layers.addText(textContent);
     textLayer.name = textName;
     textLayer.property("Position").setValue(position);
+
+    if (justification == undefined) {
+        justification = ParagraphJustification.CENTER_JUSTIFY;
+    }
+
+    var textDocument = textLayer.property("Source Text").value;
+    textDocument.justification = justification;
+    textLayer.property("Source Text").setValue(textDocument);
 }
 
 
@@ -81,9 +120,16 @@ for (var i = 1; i <= numberOfMiddleLines; i++) {
     createDottedLine([margin, yPos], [compWidth-margin, yPos], "Horizontal Line " + i);
 }
 
-// Create five text layers below the bottom horizontal line
+// Create text layers below the bottom horizontal line
 var textYPos = compHeight - margin + textYPosFromBottomLine; // adjust textHeight as needed
 for (var i = 1; i <= xValues.length; i++) {
-    var textXPos = margin + (i * (compWidth - 2*margin) / 6);
-    createText([textXPos, textYPos], xValues[i], xValues[i]);
+    var xPos = margin + ((i-1) * (compWidth - 2*margin) / xValues.length) + compWidth * 0.01;
+    createText([xPos, textYPos], xValues[i-1],  xValues[i-1]);
+}
+
+// Create text layers to the left of the vertical line
+var textXPos = margin - textXPosFromLeftLine; // adjust textXPosFromLeftLine as needed
+for (var i = 1; i <= yValues.length; i++) {
+    var yPos = compHeight - margin - ((i-1) * (compHeight - 2*margin) / yValues.length) - compHeight * 0.01;
+    createText([textXPos, yPos], yValues[i-1], yValues[i-1], ParagraphJustification.RIGHT_JUSTIFY);
 }
