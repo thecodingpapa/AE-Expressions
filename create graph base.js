c859f6f0ac09b4df7f4a77e9debe58f95bbbab5a
thecodingpapa <yangfamilyclub@gmail.com>
2024-06-04 14:24:37 +0100

    x values

--- create graph base.js
+++ create graph base.js
@@ -5,6 +5,8 @@ var numberOfMiddleLines = 3;
 var margin = 100;
 var strokeWidth = 4;
 var strokeColor = [0, 0, 0]; // Black color
+var textYPosFromBottomLine = 30;
+var xValues = ["1990", "1995", "2000", "2005", "2010", "2015", "2020"];
 
 // Create a new composition
 var comp = app.project.items.addComp("Graph Base Lines", 1920, 1080, 1, 10, 30);
@@ -56,6 +58,13 @@ function createDottedLine(startPoint, endPoint, lineName, dashLength, gapLength)
     gap.setValue(gapLength); // Gap length
 }
 
+function createText(position, textContent, textName) {
+    var textLayer = comp.layers.addText(textContent);
+    textLayer.name = textName;
+    textLayer.property("Position").setValue(position);
+}
+
+
 // Dimensions
 var compWidth = comp.width;
 var compHeight = comp.height;
@@ -70,4 +79,11 @@ createLine([margin, compHeight - margin], [compWidth - margin, compHeight - marg
 for (var i = 1; i <= numberOfMiddleLines; i++) {
     var yPos = compHeight-margin - (i * (compHeight - 2*margin) / (numberOfMiddleLines + 1));
     createDottedLine([margin, yPos], [compWidth-margin, yPos], "Horizontal Line " + i);
+}
+
+// Create five text layers below the bottom horizontal line
+var textYPos = compHeight - margin + textYPosFromBottomLine; // adjust textHeight as needed
+for (var i = 1; i <= xValues.length; i++) {
+    var textXPos = margin + (i * (compWidth - 2*margin) / 6);
+    createText([textXPos, textYPos], xValues[i], xValues[i]);
 }
