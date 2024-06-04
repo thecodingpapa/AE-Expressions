f87f92c56ecb64866140c003f205bb100a5b5ebf
thecodingpapa <yangfamilyclub@gmail.com>
2024-06-04 22:43:47 +0100

    y text are working too. vertical and horizontal line is in graph composition now

--- create graph base.js
+++ create graph base.js
@@ -7,10 +7,10 @@ var strokeWidth = 4;
 var strokeColor = [0, 0, 0]; // Black color
 
 var textYPosFromBottomLine = 30;
-var xValues = ["1990", "1995", "2000", "2005", "2010", "2015", "2020"];
+var xValues = ["1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000"];
 
 var textXPosFromLeftLine = 20;
-var yValues = ["0", "20", "40", "60", "80", "100"];
+var yValues = ["0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"];
 
 
 
@@ -43,15 +43,20 @@ var yValues = ["0", "20", "40", "60", "80", "100"];
 // Create a new composition
 var graphComp = app.project.items.addComp("graph", width, height, 1, 10, 30);
 var linesComp = app.project.items.addComp('lines', width, height, 1, 10, 30);
-var valuesComp = app.project.items.addComp('values', width, height, 1, 10, 30);
+var xvaluesComp = app.project.items.addComp('xvalues', width, height, 1, 10, 30);
+var yvaluesComp = app.project.items.addComp('yvalues', width, height, 1, 10, 30);
 graphComp.layers.add(linesComp);
-graphComp.layers.add(valuesComp);
+graphComp.layers.add(xvaluesComp);
+graphComp.layers.add(yvaluesComp);
 
 // Function to create a shape layer with a line
-function createLine(startPoint, endPoint, lineName) {
+function createLine(comp, startPoint, endPoint, lineName) {
+    if (comp === undefined) {
+        comp = linesComp;
+    }
     var start = [startPoint[0] - width/2, startPoint[1] - height/2];
     var end = [endPoint[0] - width/2, endPoint[1] - height/2];
-    var shapeLayer = linesComp.layers.addShape();
+    var shapeLayer = comp.layers.addShape();
     shapeLayer.name = lineName;
     var shapeGroup = shapeLayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Group");
     var shapeGroupContents = shapeGroup.property("ADBE Vectors Group");
@@ -94,17 +99,14 @@ function createDottedLine(startPoint, endPoint, lineName, dashLength, gapLength)
     gap.setValue(gapLength); // Gap length
 }
 
-function createText(position, textContent, textName, justification) {
-    var textLayer = valuesComp.layers.addText(textContent);
+function createXText(position, textContent, textName) {
+    var textLayer = xvaluesComp.layers.addText(textContent);
     textLayer.name = textName;
     textLayer.property("Position").setValue(position);
 
-    if (justification == undefined) {
-        justification = ParagraphJustification.CENTER_JUSTIFY;
-    }
 
     var textDocument = textLayer.property("Source Text").value;
-    textDocument.justification = justification;
+    textDocument.justification = ParagraphJustification.CENTER_JUSTIFY;
     textLayer.property("Source Text").setValue(textDocument);
 
     // // Calculate the x distance from the center of the composition to the text
@@ -114,6 +116,24 @@ function createText(position, textContent, textName, justification) {
 
     // // Store the distance in the text layer's comment so it can be accessed later
     textLayer.comment = position[0].toString();
+}
+
+function createYText(position, textContent, textName) {
+    var textLayer = yvaluesComp.layers.addText(textContent);
+    textLayer.name = textName;
+    textLayer.property("Position").setValue(position);
+
+    var textDocument = textLayer.property("Source Text").value;
+    textDocument.justification = ParagraphJustification.RIGHT_JUSTIFY;
+    textLayer.property("Source Text").setValue(textDocument);
+
+    // // Calculate the x distance from the center of the composition to the text
+    // var compCenterX = linesComp.width / 2;
+    // var textX = position[0];
+    // var distanceFromCenter = textX - compCenterX;
+
+    // // Store the distance in the text layer's comment so it can be accessed later
+    textLayer.comment = position[0].toString() + "##" + position[1].toString();
 }
 
 
@@ -146,12 +166,12 @@ function zoomCompositionWithNull(comp, startScale, endScale, startTime, endTime)
     
 }
 
-function zoomXvalues(comp) {
+function zoomXvalues() {
     // Add expression to each text layer
-    for (var i = 1; i <= comp.numLayers; i++) {
-        var layer = comp.layer(i);
+    for (var i = 1; i <= xvaluesComp.numLayers; i++) {
+        var layer = xvaluesComp.layer(i);
 
-        var initialPosX = parseFloat(layer.comment);
+        var initialPosX = parseFloat(layer.comment.split("##")[0]);
 
         // Add expression to the Position property
         layer.property("Position").expression =
@@ -166,15 +186,35 @@ function zoomXvalues(comp) {
 }
 
 
+function zoomYvalues() {
+    // Add expression to each text layer
+    for (var i = 1; i <= yvaluesComp.numLayers; i++) {
+        var layer = yvaluesComp.layer(i);
+
+        var initialPosY = parseFloat(layer.comment.split("##")[1]);
+
+        // Add expression to the Position property
+        layer.property("Position").expression =
+        'var scaleControlLayer = comp("graph").layer("Scale Control");\n'+
+        'var endScaleValue = scaleControlLayer.scale[1];\n'+
+        'var scaleFactor = endScaleValue / 100; // Assuming the start scale is 100%\n'+
+        'var anchorValue = scaleControlLayer.anchorPoint[1] + '+height/2+';\n'+
+        'var moveDistance = ('+initialPosY+' - anchorValue) * scaleFactor;\n'+
+        'value = [thisLayer.position[0], moveDistance + anchorValue];';
+
+    }
+}
+
+
 // Dimensions
 var compWidth = linesComp.width;
 var compHeight = linesComp.height;
 
 // Create left vertical line
-createLine([margin, margin], [margin, compHeight - margin], "Left Vertical Line");
+createLine(graphComp, [margin, margin], [margin, compHeight - margin], "Left Vertical Line");
 
 // Create bottom horizontal line
-createLine([margin, compHeight - margin], [compWidth - margin, compHeight - margin], "Bottom Horizontal Line");
+createLine(graphComp, [margin, compHeight - margin], [compWidth - margin, compHeight - margin], "Bottom Horizontal Line");
 
 // Create three horizontal lines spread through
 for (var i = 1; i <= numberOfMiddleLines; i++) {
@@ -186,17 +226,18 @@ for (var i = 1; i <= numberOfMiddleLines; i++) {
 var textYPos = compHeight - margin + textYPosFromBottomLine; // adjust textHeight as needed
 for (var i = 1; i <= xValues.length; i++) {
     var xPos = margin + ((i-1) * (compWidth - 2*margin) / xValues.length) + compWidth * 0.01;
-    createText([xPos, textYPos], xValues[i-1],  xValues[i-1]);
+    createXText([xPos, textYPos], xValues[i-1],  xValues[i-1]);
 }
 
 // Create text layers to the left of the vertical line
 var textXPos = margin - textXPosFromLeftLine; // adjust textXPosFromLeftLine as needed
 for (var i = 1; i <= yValues.length; i++) {
     var yPos = compHeight - margin - ((i-1) * (compHeight - 2*margin) / yValues.length) - compHeight * 0.01;
-    createText([textXPos, yPos], yValues[i-1], yValues[i-1], ParagraphJustification.RIGHT_JUSTIFY);
+    createYText([textXPos, yPos], yValues[i-1], yValues[i-1]);
 }
 
 // Apply zoom animation to linesComp
 zoomCompositionWithNull(graphComp, [100, 100], [200, 200], 1, 2); // Zoom from 100% to 200% from 1s to 2s
 
-zoomXvalues(valuesComp);
+zoomXvalues();
+zoomYvalues();
